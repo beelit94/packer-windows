e66b443cb66d01e0437316499efccf835955fa8e
Freddy Tan <beelit94@gmail.com>
2014-09-04 08:23:00 +0800

    reboot after install powershell

--- scripts/powershell.bat
+++ scripts/powershell.bat
@@ -2,10 +2,10 @@
 
 IF EXIST "%PROGRAMFILES(X86)%" (
 	powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.microsoft.com/download/3/D/6/3D61D262-8549-4769-A660-230B67E15B25/Windows6.1-KB2819745-x86-MultiPkg.msu', 'C:\Windows\Temp\Windows6.1-KB2819745-x86-MultiPkg.msu')" <NUL
-	wusa.exe C:\Windows\Temp\Windows6.1-KB2819745-x86-MultiPkg.msu /quiet /forcerestart
+	wusa.exe C:\Windows\Temp\Windows6.1-KB2819745-x86-MultiPkg.msu /quiet /norestart
 ) ELSE (
 	powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.microsoft.com/download/3/D/6/3D61D262-8549-4769-A660-230B67E15B25/Windows6.1-KB2819745-x64-MultiPkg.msu', 'C:\Windows\Temp\Windows6.1-KB2819745-x64-MultiPkg.msu')" <NUL
-	wusa.exe C:\Windows\Temp\Windows6.1-KB2819745-x64-MultiPkg.msu /quiet /forcerestart	
+	wusa.exe C:\Windows\Temp\Windows6.1-KB2819745-x64-MultiPkg.msu /quiet /norestart	
 )
 
 shutdown /r /t 5
