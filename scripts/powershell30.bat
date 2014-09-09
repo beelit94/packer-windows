f8d4c19eb0736a3f070a29db62a684bdeabbecd9
Freddy Tan <beelit94@gmail.com>
2014-09-09 15:54:13 +0800

    1. don't install powershell 40, install 30 instead

---
+++ scripts/powershell30.bat
@@ -0,0 +1,23 @@
+:: install powershell 3.0 on windows 2008 r2 sp1 & windows 7 sp1
+
+if exist "%PROGRAMFILES(X86)%" goto :X64
+if not exist "%PROGRAMFILES(X86)%" goto :X86
+goto :DONE
+
+:X64
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.microsoft.com/download/E/7/6/E76850B8-DA6E-4FF5-8CCE-A24FC513FD16/Windows6.1-KB2506143-x64.msu', 'C:\Windows\Temp\Windows6.1-KB2506143-x64.msu')" <NUL
+wusa.exe C:\Windows\Temp\Windows6.1-KB2506143-x64.msu /quiet /norestart
+GOTO :DONE
+
+:X86
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.microsoft.com/download/E/7/6/E76850B8-DA6E-4FF5-8CCE-A24FC513FD16/Windows6.1-KB2506143-x86.msu', 'C:\Windows\Temp\Windows6.1-KB2506143-x86.msu')" <NUL
+wusa.exe C:\Windows\Temp\Windows6.1-KB2506143-x86.msu /quiet /norestart
+GOTO :DONE
+
+:DONE
+timeout /t 60
+shutdown /r /t 0
+
+
+
+
---
+++ scripts/powershell40.bat
@@ -0,0 +1,19 @@
+:: install powershell 4.0 windows 2012, windows 7, windows 2008 r2
+
+if exist "%PROGRAMFILES(X86)%" goto :X64
+if not exist "%PROGRAMFILES(X86)%" goto :X86
+goto :DONE
+
+:X64
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.microsoft.com/download/3/D/6/3D61D262-8549-4769-A660-230B67E15B25/Windows6.1-KB2819745-x64-MultiPkg.msu', 'C:\Windows\Temp\Windows6.1-KB2819745-x64-MultiPkg.msu')" <NUL
+wusa.exe C:\Windows\Temp\Windows6.1-KB2819745-x64-MultiPkg.msu /quiet /norestart
+GOTO :DONE
+
+:X86
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.microsoft.com/download/3/D/6/3D61D262-8549-4769-A660-230B67E15B25/Windows6.1-KB2819745-x86-MultiPkg.msu', 'C:\Windows\Temp\Windows6.1-KB2819745-x86-MultiPkg.msu')" <NUL
+wusa.exe C:\Windows\Temp\Windows6.1-KB2819745-x86-MultiPkg.msu /quiet /norestart
+GOTO :DONE
+
+:DONE
+timeout /t 60
+shutdown /r /t 0
