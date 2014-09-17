55ae9e317113ca7a1d0ca5fddc061d74f8f0a769
Freddy Tan <beelit94@gmail.com>
2014-09-17 16:25:42 +0800

    workaround for chocolatey under systex's firewall

--- scripts/chocopacks.bat
+++ scripts/chocopacks.bat
@@ -1,5 +1,8 @@
 :: Ensure C:\Chocolatey\bin is on the path
 set /p PATH=<C:\Windows\Temp\PATH
 
-:: Install all the things; for example:
+:: add source of http to prevent block by systex's firewall
+cmd /c choco sources add -name choco_insecure -source http://chocolatey.org/api/v2/
+
+:: Install packages
 cmd /c cinst javaruntime
