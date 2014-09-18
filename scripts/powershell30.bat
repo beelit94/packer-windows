9c3a52360a51b74aad15a7d7afdb3c68dcefd819
Freddy Tan <beelit94@gmail.com>
2014-09-18 23:40:59 +0800

    upload missing fix scripts

--- scripts/powershell30.bat
+++ scripts/powershell30.bat
@@ -6,14 +6,14 @@ goto :DONE
 
 :X64
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.microsoft.com/download/E/7/6/E76850B8-DA6E-4FF5-8CCE-A24FC513FD16/Windows6.1-KB2506143-x64.msu', 'C:\Windows\Temp\Windows6.1-KB2506143-x64.msu')" <NUL
-wusa.exe C:\Windows\Temp\Windows6.1-KB2506143-x64.msu /quiet /norestart
+start /wait wusa C:\Windows\Temp\Windows6.1-KB2506143-x64.msu /quiet /norestart
 GOTO :DONE
 
 :X86
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.microsoft.com/download/E/7/6/E76850B8-DA6E-4FF5-8CCE-A24FC513FD16/Windows6.1-KB2506143-x86.msu', 'C:\Windows\Temp\Windows6.1-KB2506143-x86.msu')" <NUL
-wusa.exe C:\Windows\Temp\Windows6.1-KB2506143-x86.msu /quiet /norestart
+start /wait wusa C:\Windows\Temp\Windows6.1-KB2506143-x86.msu /quiet /norestart
 GOTO :DONE
 
 :DONE
-timeout /t 60
-shutdown /r /t 0
+
+shutdown /r /t 5
--- scripts/powershell40.bat
+++ scripts/powershell40.bat
@@ -6,14 +6,13 @@ goto :DONE
 
 :X64
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.microsoft.com/download/3/D/6/3D61D262-8549-4769-A660-230B67E15B25/Windows6.1-KB2819745-x64-MultiPkg.msu', 'C:\Windows\Temp\Windows6.1-KB2819745-x64-MultiPkg.msu')" <NUL
-wusa.exe C:\Windows\Temp\Windows6.1-KB2819745-x64-MultiPkg.msu /quiet /norestart
+start /wait wusa C:\Windows\Temp\Windows6.1-KB2819745-x64-MultiPkg.msu /quiet /norestart
 GOTO :DONE
 
 :X86
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.microsoft.com/download/3/D/6/3D61D262-8549-4769-A660-230B67E15B25/Windows6.1-KB2819745-x86-MultiPkg.msu', 'C:\Windows\Temp\Windows6.1-KB2819745-x86-MultiPkg.msu')" <NUL
-wusa.exe C:\Windows\Temp\Windows6.1-KB2819745-x86-MultiPkg.msu /quiet /norestart
+start /wait wusa C:\Windows\Temp\Windows6.1-KB2819745-x86-MultiPkg.msu /quiet /norestart
 GOTO :DONE
 
 :DONE
-timeout /t 60
-shutdown /r /t 0
+shutdown /r /t 5
